d1d2b95ce9edcf3486022b1d72ac642d97cecaa4
Barry Yu <j462yu@uwaterloo.ca>
2023-10-14 15:06:28 -0400

    Initial commit

--- app/src/main/java/ca/uwaterloo/cs346project/MainActivity.kt
+++ app/src/main/java/ca/uwaterloo/cs346project/MainActivity.kt
@@ -1,6 +1,7 @@
 package ca.uwaterloo.cs346project
 
 import android.annotation.SuppressLint
+import android.app.appsearch.SearchResults
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -24,6 +25,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ca.uwaterloo.cs346project.ui.theme.Cs346projectTheme
+import androidx.compose.runtime.*
+
 
 
 class MainActivity : ComponentActivity() {
@@ -32,13 +35,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             Cs346projectTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    //LoginPage()
                     HomePage()
+                    //CMI()
                 }
             }
         }
     }
 }
+
 
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
@@ -85,6 +89,7 @@ fun LoginPage() {
             onClick = {
                 // Handle login action here
                 keyboardController?.hide()
+
             },
             modifier = Modifier
                 .fillMaxWidth()
@@ -105,6 +110,7 @@ fun LoginPage() {
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomePage() {
     var isExpanded by remember { mutableStateOf(false) }
@@ -112,6 +118,14 @@ fun HomePage() {
     val buttonModifier = Modifier
         .fillMaxWidth()
         .padding(8.dp)
+
+    var showColumn1 by remember { mutableStateOf(true) }
+    var showColumn2 by remember { mutableStateOf(false) }
+    var showColumn3 by remember { mutableStateOf(false) }
+    var showColumn4 by remember { mutableStateOf(false) }
+    var showColumn5 by remember { mutableStateOf(false) }
+
+
 
     Column(
         modifier = Modifier.fillMaxSize()
@@ -131,29 +145,65 @@ fun HomePage() {
                     .fillMaxWidth()
             ) {
                 Button(
-                    onClick = { /* Handle button click */ },
+                    onClick = {
+                        showColumn1 = true
+                        showColumn2 = false
+                        showColumn3 = false
+                        showColumn4 = false
+                        showColumn5 = false
+                        isExpanded = false
+                    },
                     modifier = buttonModifier
                 ) {
                     Text("Schedule of Courses", fontSize = 24.sp)
                 }
+
+
                 Button(
-                    onClick = { /* Handle button click */ },
+                    onClick = {
+                        showColumn1 = false
+                        showColumn2 = true
+                        showColumn3 = false
+                        showColumn4 = false
+                        showColumn5 = false
+                        isExpanded = false
+                    },
                     modifier = buttonModifier
                 ) {
                     Text("Course Materials and Info", fontSize = 24.sp)
                 }
+
+
                 Button(
-                    onClick = { /* Handle button click */ },
+                    onClick = {
+                        showColumn1 = false
+                        showColumn2 = false
+                        showColumn3 = true
+                        showColumn4 = false
+                        showColumn5 = false
+                        isExpanded = false
+                    },
                     modifier = buttonModifier
                 ) {
                     Text("Ratings for Courses", fontSize = 24.sp)
                 }
+
+
                 Button(
-                    onClick = { /* Handle button click */ },
+                    onClick = {
+                        showColumn1 = false
+                        showColumn2 = false
+                        showColumn3 = false
+                        showColumn4 = true
+                        showColumn5 = false
+                        isExpanded = false
+                    },
                     modifier = buttonModifier
                 ) {
                     Text("Mode", fontSize = 24.sp)
                 }
+
+
                 Button(
                     onClick = { /* Handle button click */ },
                     modifier = buttonModifier
@@ -162,22 +212,106 @@ fun HomePage() {
                 }
             }
         }
+
+        if (showColumn1) {
+            Text("Course Schedule")
+        }
+
+        if (showColumn2) {
+            CMI()
+        }
+
+        if (showColumn3) {
+            Ratings()
+        }
+
+        if (showColumn4) {
+            Text("Mode")
+        }
+
+        if (showColumn5) {
+            // here should quit to loginpage
+            Text("Quit")
+        }
     }
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CMI() {}
+fun CMI() {
+    var tasks by remember { mutableStateOf(mutableMapOf<String, String>()) }
+    var dayOfWeek by remember { mutableStateOf("") }
+    var taskText by remember { mutableStateOf("") }
+
+    var isExpanded by remember { mutableStateOf(false) }
 
 
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        FloatingActionButton(
+            onClick = {isExpanded = !isExpanded
+                if (dayOfWeek.isNotEmpty() && taskText.isNotEmpty()) {
+                    tasks[dayOfWeek] = taskText
+                    dayOfWeek = ""
+                    taskText = ""
+                }
+            },
+            modifier = Modifier.align(Alignment.BottomEnd).padding(16.dp)
+        ) {
+            Text(if (isExpanded) "-" else "+")
+        }
+
+        // Use AnimatedVisibility to animate the buttons
+        AnimatedVisibility(visible = isExpanded) {
+            OutlinedTextField(
+                value = dayOfWeek,
+                onValueChange = { dayOfWeek = it },
+                placeholder = { Text("Enter Course Name") },
+                modifier = Modifier.padding(16.dp).fillMaxWidth()
+            )
+        }
+
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            tasks.forEach { (day, task) ->
+                Text(
+                    text = "$day: $task",
+                    fontSize = 18.sp,
+                    color = Color.Black
+                )
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun rating() {}
+fun Ratings() {
+
+    var searchText by remember { mutableStateOf("") }
+    var searchResults by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        OutlinedTextField(
+            value = searchText,
+            onValueChange = { searchText = it },
+            placeholder = { Text("Search by Course Name") },
+            modifier = Modifier.padding(16.dp)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+        if (searchText.isNotEmpty()) {
+            Text("Search Results for: $searchText", fontSize = 18.sp, color = Color.Black, modifier = Modifier.padding(16.dp))
+           // Add your search result UI here
+        }
+    }
+}
 
 
-@Composable
-fun Mode() {}
-
-
-@Composable
-fun Quit() {}
 
