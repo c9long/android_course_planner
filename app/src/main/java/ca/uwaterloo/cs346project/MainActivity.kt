e13beb54d3327b49b9470a414a3a728ae73449a9
dgrinton <dgrinton@live.ca>
2023-10-15 22:48:38 -0400

    Added search bar with hard-coded course suggestions.

--- app/src/main/java/ca/uwaterloo/cs346project/MainActivity.kt
+++ app/src/main/java/ca/uwaterloo/cs346project/MainActivity.kt
@@ -3,13 +3,38 @@ package ca.uwaterloo.cs346project
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ca.uwaterloo.cs346project.ui.theme.Cs346projectTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +44,15 @@ class MainActivity : ComponentActivity() {
             Cs346projectTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Title("Placeholder")
+                    Box(
+                        modifier = Modifier.fillMaxSize().padding(top = 135.dp, start = 15.dp, end = 15.dp)
+                    ) {
+                        Text(
+                            text = "View course schedule information and reviews from UW students"
+                        )
+                    }
+                    Search(listOf("CS348", "CS246", "ECON201", "SCI206"))
                 }
             }
         }
@@ -39,5 +72,62 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     Cs346projectTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun Title(title: String) {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .padding(15.dp)
+    ) {
+        Text(
+            text = title,
+            fontSize = 30.sp,
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Search(courses: List<String>) {
+    var state by remember { mutableStateOf("") }
+
+    Column( modifier = Modifier.fillMaxSize().padding(75.dp)) {
+        TextField(
+            value = state,
+            onValueChange = { state = it },
+            label = { Text("Search") },
+            leadingIcon = { Icon(Icons.Filled.Search, contentDescription = null) },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
+        )
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            var filtered: List<String> = if (!state.isEmpty()) {
+                val result = ArrayList<String>()
+                for (course in courses) {
+                    if (course.lowercase().startsWith(state.lowercase())) {
+                        result.add(course)
+                    }
+                }
+                result
+            } else listOf()
+
+            items(filtered) {
+                selected ->
+                Row(
+                    modifier = Modifier
+                        .clickable(onClick = { /* ON CLICK */ })
+                        .background(MaterialTheme.colorScheme.onSecondary)
+                        .fillMaxWidth()
+                        .padding(PaddingValues(8.dp, 16.dp))
+                ) {
+                    Text(text = selected, fontSize = 18.sp, color = Color.Black)
+                }
+            }
+        }
     }
 }
