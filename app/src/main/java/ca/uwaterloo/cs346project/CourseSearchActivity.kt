c7a3f9ea20372b64d6e1ec7dde885c36d065f438
Dylan <dgrinton@live.ca>
2023-10-20 00:02:31 -0400

    Re-style

--- app/src/main/java/ca/uwaterloo/cs346project/CourseSearchActivity.kt
+++ app/src/main/java/ca/uwaterloo/cs346project/CourseSearchActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -50,69 +49,74 @@ class CourseSearchActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun SearchPage() {
-    // Title
-    Box(
-        Modifier
-            .fillMaxSize()
-            .padding(15.dp)
-    ) {
-        Text(
-            text = "Schedule of Courses",
-            fontSize = 30.sp,
-            modifier = Modifier
-                .align(Alignment.TopCenter)
-        )
-    }
-
-    // Text under search bar
-    Box(
-        modifier = Modifier.fillMaxSize().padding(top = 135.dp, start = 15.dp, end = 15.dp)
-    ) {
-        Text(
-            text = "View course schedule information and reviews from UW students"
-        )
-    }
-
-    // Search Bar
-    Search(listOf("CS348", "CS246", "ECON201", "SCI206"))
-}
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Search(courses: List<String>) {
+fun SearchPage() {
     var state by remember { mutableStateOf("") }
+    val courses: List<String> = listOf("CS111")
 
-    Column(modifier = Modifier.fillMaxSize().padding(75.dp)) {
-        TextField(
-            value = state,
-            onValueChange = { state = it },
-            label = { Text("Search") },
-            leadingIcon = { Icon(Icons.Filled.Search, contentDescription = null) },
-            singleLine = true,
-            modifier = Modifier.fillMaxWidth()
-        )
-        LazyColumn(modifier = Modifier.fillMaxSize()) {
-            val filtered: List<String> = if (!state.isEmpty()) {
-                val result = ArrayList<String>()
-                for (course in courses) {
-                    if (course.lowercase().startsWith(state.lowercase())) {
-                        result.add(course)
+    MaterialTheme (
+        colorScheme = lightColorScheme(
+            primary = Color(0xFF0056b3),
+            secondary = Color(0xFF333333)
+        ),
+        typography = Typography(),
+        shapes = Shapes()
+    ) {
+        // Title
+        Column (
+            modifier = Modifier.padding(
+                start = 16.dp,
+                top = 16.dp,
+                end = 16.dp,
+                bottom = 16.dp
+            )
+        ) {
+            Text(
+                text = "Schedule of Courses",
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            // Search Bar
+            TextField(
+                value = state,
+                onValueChange = { state = it },
+                placeholder = { Text("Search") },
+                leadingIcon = { Icon(Icons.Filled.Search, contentDescription = null) },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Box() {
+                // Text under search bar
+                Text(text = "View course schedule information and reviews from UW students", style = MaterialTheme.typography.bodySmall,
+                    modifier = Modifier.padding(top = 8.dp))
+
+                // Search Suggestions
+                LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                    val filtered: List<String> = if (!state.isEmpty()) {
+                        val result = ArrayList<String>()
+                        for (course in courses) {
+                            if (course.lowercase().startsWith(state.lowercase())) {
+                                result.add(course)
+                            }
+                        }
+                        result
+                    } else listOf()
+
+                    items(filtered) { selected ->
+                        Row(
+                            modifier = Modifier
+                                .clickable(onClick = { /* ON CLICK */ })
+                                .background(Color.LightGray)
+                                .fillMaxWidth()
+                                .padding(PaddingValues(12.dp, 16.dp))
+                        ) {
+                            Text(text = selected, fontSize = 18.sp, color = Color.Black)
+                        }
                     }
-                }
-                result
-            } else listOf()
-
-            items(filtered) { selected ->
-                Row(
-                    modifier = Modifier
-                        .clickable(onClick = { /* ON CLICK */ })
-                        .background(MaterialTheme.colorScheme.onSecondary)
-                        .fillMaxWidth()
-                        .padding(PaddingValues(8.dp, 16.dp))
-                ) {
-                    Text(text = selected, fontSize = 18.sp, color = Color.Black)
                 }
             }
         }
