40468db9d9cfaaca5648464820629e71c4bc5188
c9long <109810526+c9long@users.noreply.github.com>
2023-10-20 00:18:50 -0400

    completed schedule

--- app/src/main/java/ca/uwaterloo/cs346project/ScheduleActivity.kt
+++ app/src/main/java/ca/uwaterloo/cs346project/ScheduleActivity.kt
@@ -1,99 +1,335 @@
 package ca.uwaterloo.cs346project
 
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.material3.Text
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.ParentDataModifier
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
-import java.time.DayOfWeek
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+import ca.uwaterloo.cs346project.ui.theme.Cs346projectTheme
 import java.time.LocalDate
-import java.time.format.TextStyle
-import java.util.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.clickable
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatter.ISO_LOCAL_DATE
+import java.time.temporal.ChronoUnit
+import kotlin.math.roundToInt
 
+
+data class Event(
+    val name: String,
+    val color: Color,
+    val start: LocalDateTime,
+    val end: LocalDateTime,
+    val description: String? = null,
+)
+
+val EventTimeFormatter = DateTimeFormatter.ofPattern("h:mm a")
 @Composable
-fun WeeklyScheduler() {
-    val today = LocalDate.now()
-    var selectedDate by remember { mutableStateOf(today) }
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        // Display the current week
-        Text(
-            text = "Week of ${selectedDate.getWeekLabel()}",
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(16.dp)
-        )
-
-        // Weekdays header
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            for (day in DayOfWeek.values()) {
-                Text(
-                    text = day.getDisplayName(TextStyle.SHORT, Locale.getDefault()),
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.weight(1f)
-                )
-            }
-        }
-
-        // Week view
-        Row(
-            modifier = Modifier.fillMaxWidth().height(100.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            for (day in selectedDate.getWeekDays()) {
-                DayCell(
-                    date = day,
-                    isSelected = day == selectedDate,
-                    onDateSelected = { selectedDate = it }
-                )
-            }
-        }
-    }
-}
-
-@Composable
-fun DayCell(
-    date: LocalDate,
-    isSelected: Boolean,
-    onDateSelected: (LocalDate) -> Unit
+fun BasicEvent(
+    event: Event,
+    modifier: Modifier = Modifier,
 ) {
-    Box(
-        modifier = Modifier
-            .weight(1f)
-            .background(if (isSelected) Color.Blue else Color.Transparent)
-            .clickable { onDateSelected(date) },
-        contentAlignment = Alignment.Center
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(end = 2.dp, bottom = 2.dp)
+            .background(event.color, shape = RoundedCornerShape(4.dp))
+            .padding(4.dp)
     ) {
         Text(
-            text = date.dayOfMonth.toString(),
-            fontSize = 16.sp,
-            color = if (isSelected) Color.White else Color.Black
+            text = "${event.start.format(EventTimeFormatter)} - ${event.end.format(EventTimeFormatter)}",
+            style = MaterialTheme.typography.labelLarge,
+        )
+
+        Text(
+            text = event.name,
+            style = MaterialTheme.typography.bodyLarge,
+            fontWeight = FontWeight.Bold,
+        )
+
+        if (event.description != null) {
+            Text(
+                text = event.description,
+                style = MaterialTheme.typography.bodyMedium,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+            )
+        }
+    }
+}
+
+private val sampleEvents = listOf(
+    Event(
+        name = "CS 111",
+        color = Color(0xFFAFBBF2),
+        start = LocalDateTime.parse("2021-05-18T13:00:00"),
+        end = LocalDateTime.parse("2021-05-18T15:00:00"),
+        description = "An arbitrary CS course placeholder description.",
+    ),
+    Event(
+        name = "CS 112",
+        color = Color(0xFFAFBBF2),
+        start = LocalDateTime.parse("2021-05-18T15:15:00"),
+        end = LocalDateTime.parse("2021-05-18T16:00:00"),
+        description = "An arbitrary CS course placeholder description.",
+    ),
+    Event(
+        name = "CS 121",
+        color = Color(0xFF1B998B),
+        start = LocalDateTime.parse("2021-05-18T16:50:00"),
+        end = LocalDateTime.parse("2021-05-18T17:00:00"),
+        description = "An arbitrary CS course placeholder description.",
+    ),
+    Event(
+        name = "CS 122",
+        color = Color(0xFFF4BFDB),
+        start = LocalDateTime.parse("2021-05-19T09:30:00"),
+        end = LocalDateTime.parse("2021-05-19T11:00:00"),
+        description = "An arbitrary CS course placeholder description.",
+    ),
+    Event(
+        name = "CS 135",
+        color = Color(0xFF6DD3CE),
+        start = LocalDateTime.parse("2021-05-19T11:00:00"),
+        end = LocalDateTime.parse("2021-05-19T12:15:00"),
+        description = "An arbitrary CS course placeholder description.",
+    ),
+    Event(
+        name = "CS 136",
+        color = Color(0xFF1B998B),
+        start = LocalDateTime.parse("2021-05-20T12:00:00"),
+        end = LocalDateTime.parse("2021-05-20T13:00:00"),
+        description = "An arbitrary CS course placeholder description.",
+    ),
+)
+
+class ScheduleActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Cs346projectTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Cs346projectTheme {
+                        Schedule(sampleEvents)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BasicSchedule(
+    events: List<Event>,
+    modifier: Modifier = Modifier,
+    eventContent: @Composable (event: Event) -> Unit = { BasicEvent(event = it) },
+    minDate: LocalDate = events.minByOrNull(Event::start)!!.start.toLocalDate(),
+    maxDate: LocalDate = events.maxByOrNull(Event::end)!!.end.toLocalDate(),
+    dayWidth: Dp,
+    hourHeight: Dp,
+) {
+    val numDays = ChronoUnit.DAYS.between(minDate, maxDate).toInt() + 1
+    Layout(
+        content = {
+            events.sortedBy(Event::start).forEach { event ->
+                Box(modifier = Modifier.eventData(event)) {
+                    eventContent(event)
+                }
+            }
+        },
+        modifier = modifier,
+    ) { measureables, constraints ->
+        val height = hourHeight.roundToPx() * 24
+        val width = dayWidth.roundToPx() * numDays
+        val placeablesWithEvents = measureables.map { measureable ->
+            val event = measureable.parentData as Event
+            val eventDurationMinutes = ChronoUnit.MINUTES.between(event.start, event.end)
+            val eventHeight = ((eventDurationMinutes / 60f) * hourHeight.toPx()).roundToInt()
+            val placeable = measureable.measure(constraints.copy(minWidth = dayWidth.roundToPx(), maxWidth = dayWidth.roundToPx(), minHeight = eventHeight, maxHeight = eventHeight))
+            Pair(placeable, event)
+        }
+        layout(width, height) {
+            placeablesWithEvents.forEach { (placeable, event) ->
+                val eventOffsetMinutes = ChronoUnit.MINUTES.between(LocalTime.MIN, event.start.toLocalTime())
+                val eventY = ((eventOffsetMinutes / 60f) * hourHeight.toPx()).roundToInt()
+                val eventOffsetDays = ChronoUnit.DAYS.between(minDate, event.start.toLocalDate()).toInt()
+                val eventX = eventOffsetDays * dayWidth.roundToPx()
+                placeable.place(eventX, eventY)
+            }
+        }
+    }
+}
+
+private class EventDataModifier(
+    val event: Event,
+) : ParentDataModifier {
+    override fun Density.modifyParentData(parentData: Any?) = event
+}
+
+private fun Modifier.eventData(event: Event) = this.then(EventDataModifier(event))
+
+@Composable
+fun BasicDayHeader(
+    day: LocalDate,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = day.format(ISO_LOCAL_DATE),
+        textAlign = TextAlign.Center,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(4.dp)
+    )
+}
+
+@Composable
+fun BasicDayHeaderPreview() {
+    Cs346projectTheme {
+        BasicDayHeader(day = LocalDate.now())
+    }
+}
+
+@Composable
+fun ScheduleHeader(
+    minDate: LocalDate,
+    maxDate: LocalDate,
+    dayWidth: Dp,
+    modifier: Modifier = Modifier,
+    dayHeader: @Composable (day: LocalDate) -> Unit = { BasicDayHeader(day = it) },
+) {
+    Row(modifier = modifier) {
+        val numDays = ChronoUnit.DAYS.between(minDate, maxDate).toInt() + 1
+        repeat(numDays) { i ->
+            Box(modifier = Modifier.width(dayWidth)) {
+                dayHeader(minDate.plusDays(i.toLong()))
+            }
+        }
+    }
+}
+
+@Composable
+fun ScheduleHeaderPreview() {
+    Cs346projectTheme {
+        ScheduleHeader(
+            minDate = LocalDate.now(),
+            maxDate = LocalDate.now().plusDays(5),
+            dayWidth = 256.dp,
         )
     }
 }
 
-fun LocalDate.getWeekDays(): List<LocalDate> {
-    val weekStart = this.with(DayOfWeek.MONDAY)
-    return (0 until 7).map { weekStart.plusDays(it.toLong()) }
+@Composable
+fun Schedule(
+    events: List<Event>,
+    modifier: Modifier = Modifier,
+    eventContent: @Composable (event: Event) -> Unit = { BasicEvent(event = it) },
+    minDate: LocalDate = events.minByOrNull(Event::start)!!.start.toLocalDate(),
+    maxDate: LocalDate = events.maxByOrNull(Event::end)!!.end.toLocalDate(),
+) {
+    val dayWidth = 256.dp
+    val hourHeight = 64.dp
+    val verticalScrollState = rememberScrollState()
+    val horizontalScrollState = rememberScrollState()
+    var sidebarWidth by remember { mutableStateOf(0) }
+    Column(modifier = modifier) {
+        ScheduleHeader(
+            minDate = minDate,
+            maxDate = maxDate,
+            dayWidth = dayWidth,
+            modifier = Modifier
+                .padding(start = with(LocalDensity.current) { sidebarWidth.toDp() })
+                .horizontalScroll(horizontalScrollState)
+        )
+        Row(modifier = Modifier.weight(1f)) {
+            ScheduleSidebar(
+                hourHeight = hourHeight,
+                modifier = Modifier
+                    .verticalScroll(verticalScrollState)
+                    .onGloballyPositioned { sidebarWidth = it.size.width }
+            )
+            BasicSchedule(
+                events = events,
+                eventContent = eventContent,
+                minDate = minDate,
+                maxDate = maxDate,
+                dayWidth = dayWidth,
+                hourHeight = hourHeight,
+                modifier = Modifier
+                    .weight(1f)
+                    .verticalScroll(verticalScrollState)
+                    .horizontalScroll(horizontalScrollState)
+            )
+        }
+    }
 }
 
-fun LocalDate.getWeekLabel(): String {
-    val startDate = this.with(DayOfWeek.MONDAY)
-    val endDate = startDate.plusDays(6)
-    return "${startDate.month.getDisplayName(TextStyle.FULL, Locale.getDefault())} " +
-            "${startDate.dayOfMonth} - ${endDate.dayOfMonth}, ${startDate.year}"
+private val HourFormatter = DateTimeFormatter.ofPattern("h a")
+
+@Composable
+fun BasicSidebarLabel(
+    time: LocalTime,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = time.format(HourFormatter),
+        modifier = modifier
+            .fillMaxHeight()
+            .padding(4.dp)
+    )
+}
+
+@Composable
+fun BasicSidebarLabelPreview() {
+    Cs346projectTheme {
+        BasicSidebarLabel(time = LocalTime.NOON, Modifier.sizeIn(maxHeight = 64.dp))
+    }
+}
+
+@Composable
+fun ScheduleSidebar(
+    hourHeight: Dp,
+    modifier: Modifier = Modifier,
+    label: @Composable (time: LocalTime) -> Unit = { BasicSidebarLabel(time = it) },
+) {
+    Column(modifier = modifier) {
+        val startTime = LocalTime.MIN
+        repeat(24) { i ->
+            Box(modifier = Modifier.height(hourHeight)) {
+                label(startTime.plusHours(i.toLong()))
+            }
+        }
+    }
+}
+
+@Composable
+fun ScheduleSidebarPreview() {
+    Cs346projectTheme {
+        ScheduleSidebar(hourHeight = 64.dp)
+    }
 }
