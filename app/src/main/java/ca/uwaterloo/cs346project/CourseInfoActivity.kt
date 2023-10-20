5fa3a981cc6be8397de75ca1767e404611b226f9
Owen Wong <owen.wong0627@gmail.com>
2023-10-19 22:49:44 -0400

    Remove Search Bar and Add Return Button

--- app/src/main/java/ca/uwaterloo/cs346project/CourseInfoActivity.kt
+++ app/src/main/java/ca/uwaterloo/cs346project/CourseInfoActivity.kt
@@ -1,28 +1,22 @@
 package ca.uwaterloo.cs346project
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ca.uwaterloo.cs346project.ui.theme.Cs346projectTheme
@@ -44,16 +38,23 @@ class CourseInfoActivity : ComponentActivity() {
                     val courseOfferings =
                         intent.getStringArrayListExtra("COURSE_OFFERING") ?: emptyList()
 
-                    var updatedCourseCode by remember { mutableStateOf(courseCode) }
-
-                    // Display course information using MaterialTheme
-                    CMI { updatedCourseCode = it }
                     CourseInfoScreen(
-                        updatedCourseCode, courseName, courseDescription, instructorName, courseOfferings
+                        courseCode, courseName, courseDescription, instructorName,
+                        courseOfferings, onBackButtonClick = { navigateBackToMainActivity() }
                     )
                 }
             }
+            BackHandler {
+                navigateBackToMainActivity()
+            }
         }
+    }
+    private fun navigateBackToMainActivity() {
+        // Create an Intent to navigate back to MainActivity
+        val intent = Intent(this, HomePageActivity::class.java)
+
+        startActivity(intent)
+        finish()
     }
 }
 
@@ -65,28 +66,21 @@ fun CourseInfoScreen(
     courseDescription: String,
     instructorName: String,
     courseOfferings: List<String>,
+    onBackButtonClick: () -> Unit,
 ) {
     MaterialTheme (
-        colorScheme = lightColorScheme(
-            primary = Color(0xFF0056b3),
-            secondary = Color(0xFF333333)
-        ),
         typography = Typography(),
         shapes = Shapes()
     ) {
         Column(
-            modifier = Modifier
-                .padding(
-                    start = 16.dp,
-                    top = 140.dp,
-                    end = 16.dp,
-                    bottom = 16.dp
-                )
+            modifier = Modifier.padding(16.dp)
         ) {
             Text(
                 text = "$courseCode: $courseName",
                 style = MaterialTheme.typography.titleLarge,
-                color = MaterialTheme.colorScheme.primary
+                color = MaterialTheme.colorScheme.primary,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
             )
             Text(text = courseDescription, style = MaterialTheme.typography.bodySmall)
             Text(
@@ -124,44 +118,13 @@ fun CourseInfoScreen(
                     }
                 }
             }
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun CMI(onUpdateCourseInfo: (String) -> Unit) {
-    var courseName by remember { mutableStateOf("") }
-
-    Box(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        // Use AnimatedVisibility to animate the buttons
-        AnimatedVisibility(visible = true) {
-            Column(
+            Button(
+                onClick = { onBackButtonClick() },
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(16.dp)
             ) {
-                OutlinedTextField(
-                    value = courseName,
-                    onValueChange = { courseName = it },
-                    placeholder = { Text("Enter Course Name") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-
-                Spacer(modifier = Modifier.height(12.dp))
-
-                // Button to update course information
-                Button(
-                    onClick = {
-                        // Call the callback function to update course information
-                        onUpdateCourseInfo(courseName)
-                    },
-                    modifier = Modifier.align(Alignment.CenterHorizontally)
-                ) {
-                    Text(text = "Update Course Info")
-                }
+                Text("Back to Main Screen")
             }
         }
     }
@@ -177,7 +140,8 @@ fun PreviewCourseInfoScreen() {
         "John Doe",
         listOf("Monday 10:00 AM - 12:00 PM",
             "Wednesday 2:00 PM - 4:00 PM",
-            "Friday 10:00 AM - 12:00 PM")
+            "Friday 10:00 AM - 12:00 PM"),
+        onBackButtonClick = {}
     )
 }
 
