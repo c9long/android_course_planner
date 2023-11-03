3e770a9fe975cc02c60fd8716bf7976201e49755
Dylan <dgrinton@live.ca>
2023-11-02 22:45:10 -0400

    Remove db debugging code

--- app/src/main/java/ca/uwaterloo/cs346project/LoginActivity.kt
+++ app/src/main/java/ca/uwaterloo/cs346project/LoginActivity.kt
@@ -52,10 +52,7 @@ fun LoginPage() {
     var SignUp by remember { mutableStateOf(false) }
 
     val dbHelper = UserDBHelper(LocalContext.current)
-    val database = dbHelper.readableDatabase
     var errorMessage by remember { mutableStateOf("") }
-
-
 
     Column(
         modifier = Modifier
