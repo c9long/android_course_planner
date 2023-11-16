174e033089768d528274a9789783c8028ff8f02e
Barry Yu <j462yu@uwaterloo.ca>
2023-11-16 18:56:00 -0500

    back to initial state

--- app/src/main/java/ca/uwaterloo/cs346project/MainActivity.kt
+++ app/src/main/java/ca/uwaterloo/cs346project/MainActivity.kt
@@ -16,27 +16,16 @@ import ca.uwaterloo.cs346project.ui.theme.Cs346projectTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // 把app卸载一下
-        System.err.println("123123")
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-            != PackageManager.PERMISSION_GRANTED) {
-            System.err.println("123123")
-            // Requesting the storage permission
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                1)
-        }
         setContent {
             Cs346projectTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     startActivity(Intent(this, LoginActivity::class.java))
-                    //startActivity(Intent(this, CourseMaterial::class.java))
                 }
             }
         }
-
-
     }
 }
+
 
 
 
