4e3b92583efb99230b481325083060362d5edbd8
Dylan <dgrinton@live.ca>
2023-11-30 18:34:11 -0500

    Move Read/Write Settings methods outside of MainActivity, to be called from other activities

--- app/src/main/java/ca/uwaterloo/cs346project/MainActivity.kt
+++ app/src/main/java/ca/uwaterloo/cs346project/MainActivity.kt
@@ -6,56 +6,48 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import ca.uwaterloo.cs346project.ui.theme.Cs346projectTheme
 import java.io.File
 import java.io.ObjectInputStream
 import java.io.ObjectOutputStream
 
 class MainActivity : ComponentActivity() {
-
     companion object {
         data class UserSettingsData(var darkMode: Boolean = false): java.io.Serializable
 
         var settings: UserSettingsData = UserSettingsData()
     }
 
-    fun readSettings() {
-        try {
-            val file = File(applicationContext.filesDir, "settings.json")
-            val stream = ObjectInputStream(file.inputStream())
-
-            settings = stream.readObject() as UserSettingsData
-            settings
-        } catch (e: Exception) {
-            println(e.message)
-        }
-    }
-
-    fun writeSettings() {
-        try {
-            val file = File(applicationContext.filesDir, "settings.json")
-            val stream = ObjectOutputStream(file.outputStream())
-
-            stream.writeObject(settings)
-        } catch (e: Exception) {
-            println(e.message)
-        }
-
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        readSettings()
         super.onCreate(savedInstanceState)
         setContent {
+            ReadSettings()
             Cs346projectTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     startActivity(Intent(this, LoginActivity::class.java))
                 }
             }
         }
-        writeSettings()
     }
+}
+
+@Composable
+fun ReadSettings() {
+    val file = File(LocalContext.current.filesDir, "settings.json")
+    val stream = ObjectInputStream(file.inputStream())
+
+    MainActivity.settings = stream.readObject() as MainActivity.Companion.UserSettingsData
+}
+
+@Composable
+fun WriteSettings() {
+    val file = File(LocalContext.current.filesDir, "settings.json")
+    val stream = ObjectOutputStream(file.outputStream())
+
+    stream.writeObject(MainActivity.settings)
 }
 
 
