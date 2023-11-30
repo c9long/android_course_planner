73a6a268aab3ba64277a6d820325f431ff59eeeb
Dylan <dgrinton@live.ca>
2023-11-30 15:39:38 -0500

    Implement dark mode, which is saved in settings file

--- app/src/main/java/ca/uwaterloo/cs346project/MainActivity.kt
+++ app/src/main/java/ca/uwaterloo/cs346project/MainActivity.kt
@@ -1,20 +1,51 @@
 package ca.uwaterloo.cs346project
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import ca.uwaterloo.cs346project.ui.theme.Cs346projectTheme
+import java.io.File
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
 
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        data class UserSettingsData(var darkMode: Boolean = false): java.io.Serializable
+
+        var settings: UserSettingsData = UserSettingsData()
+    }
+
+    fun readSettings() {
+        try {
+            val file = File(applicationContext.filesDir, "settings.json")
+            val stream = ObjectInputStream(file.inputStream())
+
+            settings = stream.readObject() as UserSettingsData
+            settings
+        } catch (e: Exception) {
+            println(e.message)
+        }
+    }
+
+    fun writeSettings() {
+        try {
+            val file = File(applicationContext.filesDir, "settings.json")
+            val stream = ObjectOutputStream(file.outputStream())
+
+            stream.writeObject(settings)
+        } catch (e: Exception) {
+            println(e.message)
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        readSettings()
         super.onCreate(savedInstanceState)
         setContent {
             Cs346projectTheme {
@@ -23,6 +54,7 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        writeSettings()
     }
 }
 
--- app/src/main/java/ca/uwaterloo/cs346project/ui/theme/Theme.kt
+++ app/src/main/java/ca/uwaterloo/cs346project/ui/theme/Theme.kt
@@ -2,7 +2,6 @@ package ca.uwaterloo.cs346project.ui.theme
 
 import android.app.Activity
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -14,6 +13,7 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import ca.uwaterloo.cs346project.MainActivity
 
 private val DarkColorScheme = darkColorScheme(
         primary = Purple80,
@@ -39,7 +39,7 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun Cs346projectTheme(
-        darkTheme: Boolean = isSystemInDarkTheme(),
+        darkTheme: Boolean = MainActivity.settings.darkMode,
         // Dynamic color is available on Android 12+
         dynamicColor: Boolean = true,
         content: @Composable () -> Unit
